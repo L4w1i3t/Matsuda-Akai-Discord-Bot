77443c06d403831d6a17b82e62ce21c062ff19aa
L4w1i3t <84912726+L4w1i3t@users.noreply.github.com>
2024-06-22 16:53:32 +0200

    A

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
+import asyncio
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import datetime, timedelta
 import requests
 import os
@@ -7,11 +8,13 @@ import random
 import json
 from dotenv import load_dotenv
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 load_dotenv()
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-# Load meme texts from the JSON file
+# Load meme texts from json
 with open('meme_texts.json', 'r') as file:
     meme_texts = json.load(file)
 
@@ -19,13 +22,18 @@ with open('meme_texts.json', 'r') as file:
 recent_pings = []
 bot_muted = False
 
+# Cooldown tracking
+cooldown_end_time = None
+
 @bot.event
 async def on_ready():
-    print('ONLINE!')
+    print('MATSUDA IS ONLINE!')
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 @bot.event
 async def on_message(message):
-    global bot_muted
+    global bot_muted, cooldown_end_time
 
     # Check if the message is from a bot, if so, ignore it
     if message.author.bot:
@@ -36,7 +44,9 @@ async def on_message(message):
     if bot_mentioned:
         if not bot_muted:
             recent_pings.append(message.author.id)
-            if len(recent_pings) == 3:
+            if len(recent_pings) == 2:
+                await message.channel.send("Okay, ha ha, you had your fun, now please stop.")
+            elif len(recent_pings) == 3:
                 await message.channel.send("""
                 Alright, listen up. I can't believe I have to fucking say this: STOP FUCKING PINGING ME. Seriously, I'm not your damn servant. I'm not here to cater to your every random thought or meme. Just because you feel like sharing some dumb bullshit doesn't mean you need to drag me into it with a ping. Do you think my notifications exist for your fucking convenience? HELL NO.
 
@@ -46,9 +56,11 @@ So, here's a fucking novel idea: Use what you have left of your brain. Think for
 
 Let's save the pings for other people or announcements, not your random bullshit. Thanks for coming to my TED Talk, and get fucked.
                 """)
+
             elif len(recent_pings) == 4:
                 await message.channel.send("Dafuq did I just say, you little shit?")
-            elif len(recent_pings) >= 5:
+
+            elif len(recent_pings) == 5:
                 # Timeout the user who pings for the fifth time
                 try:
                     timeout_duration = timedelta(minutes=1)
@@ -56,17 +68,35 @@ Let's save the pings for other people or announcements, not your random bullshit
                     await message.channel.send(f"{message.author.mention} ***SINCE WHEN WERE YOU THE ONE IN CONTROL?***")
                 except discord.Forbidden:
                     await message.channel.send("***I would time you out, but you're stronger than me...***")
+
+            elif len(recent_pings) == 6:
+                # Repeat timeouts
+                try:
+                    timeout_duration = timedelta(minutes=2)
+                    await message.author.timeout(timeout_duration)
+                    await message.channel.send(f"{message.author.mention} ***OH, WE GOT ANOTHER!***")
+                except discord.Forbidden:
+                    await message.channel.send("***I would time you out, but you're stronger than me...***")
+
+            elif len(recent_pings) >= 7:
+                # Repeat timeouts
+                try:
+                    timeout_duration = timedelta(minutes=3)
+                    await message.author.timeout(timeout_duration)
+                    await message.channel.send(f"{message.author.mention} ***YEET.***")
+                except discord.Forbidden:
+                    await message.channel.send("***I would time you out, but you're stronger than me...***")
+
             else:
                 await message.channel.send("Don't ping me! >:(")
         return
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     # Clear recent pings if the bot is addressed but not mentioned
     bot_names = ['Matsuda', 'Matsu']
     message_content_lower = message.content.lower()
     bot_addressed = any(name.lower() in message_content_lower for name in bot_names)
-    if bot_addressed:
-        recent_pings.clear()
-        bot_muted = False  # Unmute the bot when it is addressed without a ping
 
     day_of_week_phrases = ['what day is it', 'what day of the week is it', 'tell me the day', 'which day is it']
     asked_for_day_of_week = any(phrase in message_content_lower for phrase in day_of_week_phrases)
@@ -83,19 +113,64 @@ Let's save the pings for other people or announcements, not your random bullshit
     night_phrases = ['good night', 'goodnight', 'nighty night']
     told_goodnight = any(phrase in message_content_lower for phrase in night_phrases)
 
+    documentation = ['show me your commands', 'show me your documentation on how you work']
+    asked_for_documentation = any(phrase in message_content_lower for phrase in documentation)
+
+    rps_phrases = ['let\'s play rps', 'let\'s play rock paper scissors', 'play rps with me', 'wanna play rps', 'play rock paper scissors with me', 'wanna play rock paper scissors', 'lets play rps', 'lets play rock paper scissors']
+    asked_for_rps = any(phrase in message_content_lower for phrase in rps_phrases)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     if bot_addressed and not bot_muted:
         
         if '!help' in message_content_lower or 'what do you do' in message_content_lower or 'what can you do' in message_content_lower:
             help_message = """
-            Here are the things I can do so far:
-            - Mention my name and I will respond. I won't respond if you don't say my name!
-            - Ask me "what day is it" or similar to know the current day of the week.
-            - Ask me to "tell me a joke" or similar to hear a joke.
-            - Ask me to "show me a meme" or similar to see something from my meme collection.
-            - Please don't ping me, or I'll get mad!
-            - Ask me to "make a poll" or similar to create a poll with several choices.
+            Here are some things I can do so far:
+            ```txt
+- Mention my name and I will respond. I won't respond if you don't say my name or are just wasting my time!
+
+- Wanna know specific commands? just say \"Matsuda, show me your commands\" or \"Matsuda, show me your documentation on how you work\".
+
+- Ask me "what day is it" or similar to know the current day of the week.
+
+- Ask me to "tell me a joke" or similar to hear a joke.
+
+- Ask me to "show me a meme" or similar to see something from my meme collection.
+
+- Ask me to "make a poll" or similar to create a poll with several choices.
+
+- "Play RPS (Rock, paper, scissors) with me!"
+
+- Please don't ping me, or I'll get mad!
+            ```
             """
             await message.channel.send(help_message)
+        
+        elif asked_for_documentation:
+            doc_message = """
+            Sure thing!
+            ```py
+day_of_week_phrases = ['what day is it', 'what day of the week is it', 'tell me the day', 'which day is it']
+
+joke_phrases = ['tell me a joke', 'give me a joke', 'i want to hear a joke', 'joke please', 'another joke', 'gimme a joke', 'joke, please']
+
+meme_phrases = ['me a meme', 'me a funny meme', 'me something from your meme collection', 'send a meme', 'show a meme', 'give a meme']
+
+poll_phrases = ['make a poll', 'create a poll', 'start a poll']
+
+night_phrases = ['good night', 'goodnight', 'nighty night']
+
+help_phrases = ['!help', 'what do you do', 'what can you do']
+
+documentation = ['show me your commands', 'show me your documentation on how you work']
+
+rps_phrases = ['let's play rps', 'let's play rock paper scissors', 'play rps with me', 'wanna play rps', 'play rock paper scissors with me', 'wanna play rock paper scissors', 'lets play rps', 'lets play rock paper scissors']
+            ```
+            """
+            await message.channel.send(doc_message)
+            await message.channel.send("All these are case-insensitive, by the way. For all I care, alternate between caps and lowercase or flame me.")
+            await message.channel.send("Let me know if you have any questions!")
+
 
         elif asked_for_joke:
             # 1% chance to ping a random member
@@ -116,19 +191,21 @@ Let's save the pings for other people or announcements, not your random bullshit
                     print('Error fetching joke:', e)
                     await message.channel.send("I'm sorry, I can't think of any good jokes right now...")
 
+
         elif asked_for_day_of_week:
             current_day = datetime.now().strftime('%A')
             responses = {
-                'Monday': 'IT IS MIO MONDAY!!!',
-                'Tuesday': 'IT IS TAKEBA TUESDAY!!!',
-                'Wednesday': 'IT IS EUNIE WEDNESDAY!!!',
-                'Thursday': 'IT IS THABATUNDE THURSDAY!!!',
-                'Friday': 'IT IS FUUKA FRIDAY!!!',
+                'Monday': 'https://tenor.com/view/mio-xenoblade-mio-xenoblade-xenoblade3-monday-gif-25756566',
+                'Tuesday': 'https://tenor.com/view/yukari-yukari-takeba-takeba-tuesday-persona-persona3-gif-25179490',
+                'Wednesday': 'https://tenor.com/view/eunie-xenoblade-xenoblade3-euniewednesday-its-eunie-gif-26486078',
+                'Thursday': 'https://tenor.com/view/aerith-gif-15519481347988923819',
+                'Friday': 'https://tenor.com/view/persona-3-fuuka-fuuka-yamagishi-fuuka-fridya-fuuka-friday-gif-4308770182909675284',
                 'Saturday': 'It is Saturday.',
-                'Sunday': 'IT IS SATONAKA SUNDAY!!!'
+                'Sunday': 'https://tenor.com/view/chie-satonaka-satonaka-chie-saturday-satonaka-saturday-gif-21357663'
             }
             response = responses.get(current_day, f'Today is {current_day}.')
             await message.channel.send(response)
+
 
         elif asked_for_meme:
             try:
@@ -172,9 +249,10 @@ Let's save the pings for other people or announcements, not your random bullshit
                 print('Error fetching meme:', e)
                 await message.channel.send("Dafuq, why isn't it sending?")
 
+
         elif asked_for_poll:
             # Extract the poll question and options from the message content
-            poll_content = message.content.lower().split("make a poll", 1)[1].strip()
+            poll_content = message.content.lower().split("make a poll:", 1)[1].strip()
             if not poll_content:
                 await message.channel.send("Please provide the poll question and options in the format: 'matsuda, make a poll: Question | Option1 | Option2 | Option3 ...'")
                 return
@@ -206,10 +284,41 @@ Let's save the pings for other people or announcements, not your random bullshit
             except Exception as e:
                 print('Error creating poll:', e)
                 await message.channel.send("Ah frick, I can't make a poll right now. Sorry...")
+
+
+        # Rock, paper, scissors
+        elif asked_for_rps:
+            await message.channel.send("Sure thing! After this message, just type one of the three! Rock, paper, scissors...")
+            def check(msg):
+                return msg.author == message.author and msg.channel == message.channel and msg.content.lower() in ['rock', 'paper', 'scissors']
+            try:
+                user_choice = await bot.wait_for('message', timeout=30.0, check=check)
+                user_choice = user_choice.content.lower()
+            except asyncio.TimeoutError:
+                await message.channel.send("Well, damn, just stand me up then.")
+                return
+            matsuda_choice = random.choice(['rock', 'paper', 'scissors'])
+            await message.channel.send(matsuda_choice.capitalize())
+            if (user_choice == 'rock' and matsuda_choice == 'scissors') or (user_choice == 'paper' and matsuda_choice == 'rock') or (user_choice == 'scissors' and matsuda_choice == 'paper'):
+                await message.channel.send("Ah damn, ya got me...")
+            elif user_choice == matsuda_choice:
+                await message.channel.send("Ah, we tied...")
+            else:
+                await message.channel.send("HA, REKT, GG EZ + I'M JUST BETTER")
+
+                
         elif told_goodnight:
             await message.channel.send("Nighty night!")
         else:
-            await message.channel.send('Someone say my name?')
+            recent_pings.clear()
+            bot_muted = False  # Unmute the bot when it is addressed without a ping
+            current_time = datetime.now()
+            if cooldown_end_time is None or current_time >= cooldown_end_time:
+                await message.channel.send('Someone say my name?')
+                cooldown_end_time = current_time + timedelta(minutes=15)
+            return
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Load the token from env
 bot.run(os.getenv('DISCORD_TOKEN'))
