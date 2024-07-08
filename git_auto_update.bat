636185148ee0da19c16005b90f51a25fb7909e7b
L4w1i3t <84912726+L4w1i3t@users.noreply.github.com>
2024-07-08 14:34:46 +0200

    Added package files, added auto-updater to github

---
+++ git_auto_update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Added package files, added auto-updater to github"
+git push
--- main.py
+++ main.py
@@ -6,14 +6,18 @@ import os
 from logic.helper import decrypt_env
 from logic.events import setup_events
 
-# Decrypt the .env file and load environment variables
-decrypt_env()
-load_dotenv()
+def main():
+    # Decrypt the .env file and load environment variables
+    decrypt_env()
+    load_dotenv(dotenv_path='.env.decrypted')
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+    bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-# Set up events
-setup_events(bot)
+    # Set up events
+    setup_events(bot)
 
-# Load the token from env
-bot.run(os.getenv('DISCORD_TOKEN'))
+    # Load the token from env
+    bot.run(os.getenv('DISCORD_TOKEN'))
+
+if __name__ == "__main__":
+    main()
